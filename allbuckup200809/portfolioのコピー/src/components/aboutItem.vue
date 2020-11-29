5054133b115935d5251ae9b838eabd7551b5f8d4
shinyakato86 <fujiwarapoteto@gmail.com>
2020-11-29 21:52:25 +0900

    201129
    201129 commit

--- allbuckup200809/portfolioのコピー/src/components/aboutItem.vue
+++ allbuckup200809/portfolioのコピー/src/components/aboutItem.vue
@@ -31,40 +31,39 @@
                 <p class="prfileBox_title">Shinya Kato</p>
                 <p>大学卒業後、新卒で物流管理の仕事に就く。<br>
                 2018年8月より半年間Webデザインの職業訓練を受講し、Web制作会社に転職。<br>
-                マークアップエンジニアとして業務を行う。</p> 
+                マークアップエンジニアとして業務を行う。</p>
               </div>
             </div>
           </div>
           <div class="block01_item-02 block02">
             <div class="block02_item">
               <h3 class="heading03">Skill</h3>
-            </div>    
+            </div>
           <div class="block02_item-02">
             <ul class="block01List">
+              <li class="block01List_list"><p class="block01_title">PHP/laravel</p>
+              <p class="block01_text">バックエンドの部分に携わっていきたいと思い、独学で勉強中。laravelを使用したポートフォリオを複数作成しました。</p></li>
+              <li class="block01List_list">
+              <p class="block01_title">Vue.js</p>
+              <p class="block01_text">このポートフォリオを作るために使用しました、独学で勉強。</p></li>
+              <li class="block01List_list"><p class="block01_title">Git</p>
+              <p class="block01_text">Backlog、Bitbucket、Soucetreeを実務で使用。</p></li>
               <li class="block01List_list"><p class="block01_title">HTML&amp;CSS</p>
                 <p class="block01_text">Web業界へ転職しようと思った2018年1月より勉強を開始。最初はオンラインスクールを受講し、その後職業訓練校で学習。</p></li>
               <li class="block01List_list"><p class="block01_title">Sass</p>
-                <p class="block01_text">Gulpを使ったコンパイルを実務で経験。</p></li>
-              <li class="block01List_list"><p class="block01_title">jQuery</p>
+                <p class="block01_text">Gulpを使ったコンパイルを実務で使用。</p></li>
+              <li class="block01List_list"><p class="block01_title">JavaScript / jQuery</p>
                 <p class="block01_text">スライドショー、トグルメニュー、モーダル、タブ切り替え等の実装のために実務で使用。</p></li>
-              <li class="block01List_list"><p class="block01_title">JavaScript</p>
-                <p class="block01_text">jQueryと合わせて実務で使用。</p></li>
-                <li class="block01List_list"><p class="block01_title">PHP/laravel</p>
-                <p class="block01_text">バックエンドの部分に携わっていきたいと思い、独学で勉強中。laravelを使用したサービスを作成しました。</p></li>
-                <li class="block01List_list">
-                <p class="block01_title">Vue.js</p>
-                <p class="block01_text">このポートフォリオを作るために使用しました、独学で勉強。</p></li>
-              <li class="block01List_list"><p class="block01_title">Git</p>
-                <p class="block01_text">Soucetreeを使用してのバージョン管理を実務で経験。</p></li>
               <li class="block01List_list"><p class="block01_title">Adobe</p>
                   <ul class="list-style01 mt10">
                     <li>Illustrator CC</li>
                     <li>Photoshop CC</li>
                     <li>Dreamweaver CC</li>
+                    <li>XD CC</li>
                   </ul>
                 </li>
-              <li class="block01List_list"><p class="block01_title">Visual Studio Code</p>
-                <p class="block01_text">DWより動作が軽く、プラグインが豊富なため個人的な作業を行うときに使用。</p></li>
+              <li class="block01List_list"><p class="block01_title">Visual Studio Code / Atom</p>
+                <p class="block01_text">DWより動作が軽く、プラグインが豊富なため実務で使用。</p></li>
             </ul>
             </div>
         </div>
