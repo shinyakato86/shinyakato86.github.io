3bd1c915cc14c91212720b145c27debbc19b4d2a
shinyakato86 <fujiwarapoteto@gmail.com>
2021-05-24 14:46:46 +0900

    210524
    backup

---
+++ allbuckup210509/portfolioのコピー/src/views/Works.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="works">
+    <worksitem/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import worksitem from '@/components/worksItem.vue'
+
+export default {
+  name: 'works',
+  components: {
+    worksitem
+  }
+}
+</script>
